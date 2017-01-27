4ac551c3497355d5d62803b6e204fbfca6509f90
George Griffin <george@Georges-MacBook-Pro.local>
2017-01-27 02:13:18 -0500

    Overhaul of horsejump ui

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,9 +1,61 @@
+
+started_time = null;
+movecount = 0;
+gamestarted = false;
+moving = false;
+game_timer = null;
+
+function pad(n, width, z) {
+  //TGFSO
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+
+function timeConversion(millisec) {
+    var seconds = Math.floor((millisec / 1000)%60);
+    var minutes = Math.floor(millisec / (1000 * 60));
+    //var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
+    //var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
+    return pad(minutes,2) + ":" + pad(seconds,2);
+}
+
+function updateTimer(){
+	if(!gamestarted || started_time==null){
+		return // don't touch timer if game isn't running;
+	}
+	var timer_elem = document.getElementById("time-value");
+	var elapsed = new Date() - started_time;
+	var timer_string = timeConversion(elapsed);
+	timer_elem.innerText = timer_string;	
+}
+
+function resetTimer(){
+	var timer_elem = document.getElementById("time-value");
+	timer_elem.innerText = "00:00";
+}
+
+function incrementMoveCount(){
+	if(!gamestarted){return}
+	var movecount_element = document.getElementById("moves-value");
+	movecount += 1;
+	movecount_element.innerText = movecount;
+}
+
+function resetMoveCount(){
+	var movecount_element = document.getElementById("moves-value");
+	movecount = 0;
+	movecount_element.innerText = movecount;
+}
+
+
 function getBlankElem(){
 	return document.querySelector("[hj-value=\" \"]");
 }
 
 function getAllGamePieces(){
-	return document.getElementsByClassName("gamePiece");
+	return document.getElementsByClassName("playfield-piece");
 }
 
 function createCoordObj(x, y){
@@ -36,27 +88,44 @@ function getCoordsFromElem(elem){
 }
 
 function shuffle(iterations){
+	resetTimer();
+	reset();
+	started_time = new Date();
 	for(var i=0; i<iterations; i++){
 		var valids = getAllValidElems();
 		var valid = valids[Math.floor(Math.random()*valids.length)];
 		getElemByCoords(valid).click();
 	}
+	movecount = 0
+	gamestarted = true;
+	started_time = new Date();
+	game_timer = window.setInterval(updateTimer, 500);
 }
 
 function reset(){
-	var table_elem = document.getElementsByTagName("table")[0]
+	window.clearInterval(game_timer);
+	game_timer = null;
+	started_time = null;
+    resetMoveCount();
+    gamestarted = false;
+    moving = false;
+    resetTimer();
+	var table_elem = document.getElementsByClassName("playfield-contents")[0]
 	table_elem.innerHTML = "";
 	setUp();
+	//Add score keeping code her
 }
 
 function swap(elem){
+	incrementMoveCount();
 	var blank_elem = getBlankElem();
-	var blank_val = blank_elem.textContent;
 	var blank_classes = blank_elem.classList;
-	blank_elem.textContent = elem.textContent;
+	var blank_elem_label = blank_elem.getElementsByClassName("playfield-piece-label")[0];
+    var blank_val = blank_elem_label.textContent;
+	blank_elem_label.textContent = elem.textContent;
 	blank_elem.classList = elem.classList;
 	blank_elem.setAttribute('hj-value', elem.textContent)
-	elem.textContent = blank_val;
+	elem.getElementsByClassName("playfield-piece-label")[0].textContent = blank_val;
 	elem.classList = blank_classes;
 	elem.setAttribute('hj-value', blank_val);
 }
@@ -120,17 +189,27 @@ function handleClick(){
 	}
 }
 
+function pieceLabel(text){
+	var label = document.createElement("div");
+	label.innerText = text;
+	label.classList.add("playfield-piece-label");
+	return label
+}
+
+
 function setUp(){
-	var table = document.getElementsByTagName("table")[0];
+	var table = document.getElementsByClassName("playfield-contents")[0];
 	var table_rows = [[],[],[],[],[]];
 	var _cnt = 0;
 	for(y=0; y<=4; y++){
 
-		var row = document.createElement("tr");
+		// var row = document.createElement("tr");
 
 		for(x=0; x<=4; x++){
 			_cnt++;
-			var cell = document.createElement("td");
+			var space = document.createElement("div");
+			space.className = "playfield-space";
+			var cell = document.createElement("div")
 			if(_cnt == 25){
 				_cnt = " ";
 
@@ -138,17 +217,15 @@ function setUp(){
 			cell.setAttribute("hj-value",_cnt);
 			cell.setAttribute("hj-pos-x", x);
 			cell.setAttribute("hj-pos-y", y);
-			cell.classList.add("gamePiece");
-			cell.classList.add("circle");
+			cell.classList.add("playfield-piece");
 			cell.onclick = handleClick;
-			cell.appendChild(document.createTextNode(_cnt.toString()));
-			row.appendChild(cell);
-			table_rows[y].push(cell);
+			cell.appendChild(
+				pieceLabel(_cnt.toString()
+					)
+				);
+			space.appendChild(cell);
+			table.appendChild(space);
 		}
-		table.appendChild(row);
 	}
 	setValids(getCoordsFromElem(getBlankElem()));
-	return table_rows;
 }
-
-setUp();
